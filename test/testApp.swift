179043fc24fbddc43eae8f4beb0c9c0bb28b1778
erinkirstn <75390731+erinkirstn@users.noreply.github.com>
2021-08-15 11:10:04 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Erin Kirsten on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
