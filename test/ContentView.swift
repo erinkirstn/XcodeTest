45d80e7d7c9c014ad71f7d482c446d35fad7bcfb
erinkirstn <75390731+erinkirstn@users.noreply.github.com>
2021-08-20 12:17:28 -0500

    fixed bug

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    // All UI elements have to go between the two body brackets
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // A modifier is something that can change the look or behavior of a view element ex: .padding or .blur
+        
+        //VStack makes two elements go vertically stacked on top of each other.
+        //HStack Horizontally stacks
+        //ZStack stacks elements on top of each other
+        VStack(alignment: .center) {
+            
+            Spacer() // A spacer takes up as much space as it can. 2+ spacer elements take up an equal amount of space
+            
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+            
+            HStack() {
+                Spacer()
+                Text("Hello!")
+                Spacer()
+                Text("World")
+                Spacer()
+            }
+            
+            Spacer()
+    }
+        
+       
     }
 }
 
+// MARK: SECTION 2
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+           
+        }
     }
 }
